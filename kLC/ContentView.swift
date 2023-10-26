c1915eaace392514f613d1fc32d43215795ac0c7
Wesley Brito <wesleypkn@gmail.com>
2023-10-26 16:50:19 -0300

    Initial Commit

---
+++ kLC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kLC
+//
+//  Created by Wesley Brito on 26/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kLC/kLCApp.swift
@@ -0,0 +1,17 @@
+//
+//  kLCApp.swift
+//  kLC
+//
+//  Created by Wesley Brito on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct kLCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
